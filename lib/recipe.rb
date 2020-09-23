138ec7ecaf1278c50d1a5be3e4d45ab9db766f1b
Shaun James <ShaunDaneJames@Gmail.com>
2020-09-23 09:32:18 -0600

    Adds ability to add ingredients

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -4,7 +4,10 @@ class Recipe
 
   def initialize(name)
     @name                 = name
-    @ingredients_required = Hash.new
+    @ingredients_required = Hash.new(0)
   end
 
+  def add_ingredient(ingredient, quantity)
+    @ingredients_required[ingredient] += quantity
+  end
 end
--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -19,4 +19,29 @@ class RecipeTest < Minitest::Test
     assert_equal ({}), recipe1.ingredients_required
   end
 
+  def test_knows_required_ingredients
+    recipe1 = Recipe.new("Mac and Cheese")
+    ingredient1 = Ingredient.new({
+      name: "Cheese",
+      unit: "oz",
+      calories: 50
+      })
+    ingredient2 = Ingredient.new({
+      name: "Macaroni",
+      unit: "oz",
+      calories: 200
+      })
+
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient1, 4)
+    recipe1.add_ingredient(ingredient2, 8)
+
+    expected = {
+      ingredient1 => 6,
+      ingredient2 => 8
+    }
+
+    assert_equal expected, recipe1.ingredients_required
+
+  end
 end
