1f6d0fdbd7618d0a70ed527e0f8d8b091369dc7b
Shaun James <ShaunDaneJames@Gmail.com>
2020-09-23 09:45:27 -0600

    Adds ability to add recipes

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -18,4 +18,15 @@ class CookBookTest < Minitest::Test
 
     assert_equal [], cookbook.recipes
   end
+
+  def test_can_add_recipes
+    cookbook = CookBook.new
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe2 = Recipe.new("Cheese Burger")
+
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+
+    assert_equal [recipe1, recipe2], cookbook.recipes
+  end
 end
