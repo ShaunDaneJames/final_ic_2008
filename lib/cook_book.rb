8216b01dd7c8dc8664a71929c599b99fc0808469
Shaun James <ShaunDaneJames@Gmail.com>
2020-09-23 10:18:43 -0600

    Adds can list ingredients

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -7,5 +7,13 @@ class CookBook
 
   def add_recipe(recipe)
     @recipes << recipe
-  end 
+  end
+
+  def ingredients
+    @recipes.flat_map do |recipe|
+      recipe.ingredients_required.flat_map do |ingredient, _|
+          ingredient.name
+      end
+    end.uniq
+  end
 end
