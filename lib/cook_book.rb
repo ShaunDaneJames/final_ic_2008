b75be80d6373ddebc1d2cfa820dc6101f1a2b998
Shaun James <ShaunDaneJames@Gmail.com>
2020-09-23 09:42:08 -0600

    Adds cook book class

---
+++ lib/cook_book.rb
@@ -0,0 +1,7 @@
+class CookBook
+
+  def initialize
+
+  end 
+
+end
---
+++ test/cook_book_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/pantry'
+require './lib/recipe'
+require './lib/cook_book'
+
+class CookBookTest < Minitest::Test
+
+  def test_it_exists
+    cookbook = CookBook.new
+
+    assert_instance_of CookBook, cookbook
+  end
+
+end
