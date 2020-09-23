6dbcdf33654a69d21f0d35c5a80f1d588a521fcf
Shaun James <ShaunDaneJames@Gmail.com>
2020-09-23 09:29:13 -0600

    Adds ingredients required hash

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -12,4 +12,11 @@ class RecipeTest < Minitest::Test
     assert_instance_of Recipe, recipe1
     assert_equal "Mac and Cheese", recipe1.name
   end
+
+  def test_starts_with_empty_required_ingredients
+    recipe1 = Recipe.new("Mac and Cheese")
+
+    assert_equal ({}), recipe1.ingredients_required
+  end
+
 end
